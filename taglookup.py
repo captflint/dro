d7d0a49a278b5eb399ed09903781f58d35aae994
James Stephenson <c4p7.fl1n7@gmail.com>
2014-05-02 09:13:18 -0400

    optimized getsubtags() and added comments to taglookup.py

--- taglookup.py
+++ taglookup.py
@@ -1,12 +1,16 @@
+# (C) 2014 James Stephenson
+
 with open('MARC21bibfieldlist.txt', 'rt') as infile:
     tagdata = infile.read()
 
+#isnine take a MARC tag and returns True if that tag is for local use
 def isnine(tag):
     for number in tag:
         if number == '9':
             return(True)
     return(False)
 
+#gettag() takes a MARC tag and returns a string describing that tag
 def gettag(tagrequest):
     if isnine(tagrequest) is True:
         return(tagrequest)
@@ -20,18 +24,25 @@ def gettag(tagrequest):
         current = current + 1
     return(tagreply)
 
-def getsubtag(maintag, subtagrequest):
+#getsubtags() takes a MARC tag as the first argument and a list of subtags
+#as the 2nd argument.  It returns a list of strings describing the subtags
+def getsubtags(maintag, subtagrequests):
     if isnine(maintag) is True:
-        return(subtagrequest)
+        return(subtagrequests)
     current = 0
     maintag = maintag + ' - '
-    subtagrequest = '$' + subtagrequest + ' - '
-    subtagreply = ""
+    subtagreply = []
     while tagdata[current:current + 6] != maintag:
         current = current + 1
-    while tagdata[current:current + 5] != subtagrequest:
-        current = current + 1
-    while tagdata[current] != '\n':
-        subtagreply = subtagreply + tagdata[current]
-        current = current + 1
+    mainloc = current
+    for item in subtagrequests:
+        subtagrequest = '$' + item + ' - '
+        subanswer = ""
+        while tagdata[current:current + 5] != subtagrequest:
+            current = current + 1
+        while tagdata[current] != '\n':
+            subanswer = subanswer + tagdata[current]
+            current = current + 1
+        current = mainloc
+        subtagreply.append(subanswer)
     return(subtagreply)
