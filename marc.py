39f6bac8be5b9c289b6aad8f71931d6a97df9f7a
James Stephenson <c4p7.fl1n7@gmail.com>
2014-05-04 19:55:53 -0400

    Wrote a self-healing marc directory parser that even works sometimes

--- marc.py
+++ marc.py
@@ -8,13 +8,37 @@ class MARCrecord:
     record = []
 
     def parse_marc21(self):
+        self.raw_marc21 = self.raw_marc21 + '!!!!!!!!!!!!!!!'
         baseaddr = int(self.raw_marc21[12:17])
         current = 24
+        offset = 0
+        newoffset = False
+        olderoffset = 0
         while self.raw_marc21[current] != "":
             tag = self.raw_marc21[current:current + 3]
             length = int(self.raw_marc21[current + 3:current + 7])
             start = int(self.raw_marc21[current + 7:current + 12])
-            entry = self.raw_marc21[baseaddr + start:baseaddr + start + length]
+            if self.raw_marc21[baseaddr + start + length + offset - 1] != '':
+                olderoffset = offset
+                offset = 0
+                oldoffset = 0
+                newoffset = True
+                while self.raw_marc21[baseaddr + start + length + offset - 1] != '':
+                    if offset == oldoffset * -1:
+                        oldoffset = offset
+                        if offset < 0:
+                            offset = offset * -1
+                        offset = offset + 1
+                    else:
+                        oldoffset = offset
+                        offset = offset * -1
+            print('offset is', offset)
+            if newoffset is False:
+                entry = self.raw_marc21[baseaddr + start + offset:baseaddr + start + length + offset]
+            else:
+                entry = self.raw_marc21[baseaddr + start + olderoffset:baseaddr + start + length + offset]
+                newoffset = False
+            print(tag, entry)
             self.record.append([tag, entry])
             current = current + 12
         for x in range(0, len(self.record)):
